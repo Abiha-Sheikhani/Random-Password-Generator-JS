eeb7b50386f78ba7572e68720b212a3c3e74be32
Abiha-Sheikhani <abihajawed345@gmail.com>
2025-05-11 19:21:15 +0500

    done

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let spechar = prompt("Do you wanna add special characters? \n you can write yes 
 
 let loweralphabets = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 let upperalphabets = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-let specialchar = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+"]
+let specialchar = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","~",";",":","<",">","/","?"]
 let numbers = ["0","1","2","3","4","5","6","7","8","9"]
 
 let finalpool = [];
@@ -17,7 +17,7 @@ if(lowercase==="yes"){
 if(uppercase==="yes"){
     finalpool = finalpool.concat(upperalphabets)
 } 
-if(specialchar==="yes"){
+if(spechar==="yes"){
     finalpool = finalpool.concat(specialchar)
 } 
 if(number==="yes"){
@@ -25,23 +25,22 @@ if(number==="yes"){
 } 
 
 if (finalpool.length === 0) {
-    console.log("you must select atleast one condition");
+    alert("you must select atleast one condition");
     
 }
 else if (howmanychar<1 || howmanychar>30){
-    console.log("the number of characters must be between 1 and 30");
+         alert("the number of characters must be between 1 and 30");
     
 }
 else{
     var password = ""
     for(var i = 0 ; i<howmanychar ; i++){
-        var random = finalpool[Math.round(Math.random()*finalpool.length)]
+        var random = finalpool[Math.floor(Math.random()*finalpool.length)]
         password+= random
     }
-    console.log("your password is: " + password);
-    
+    document.write("<div>"+ "<h1 class = 'random'>"+"your password is: " +"<br>" + password +"</h1>"+"</div>")
+     
 }
-
 
 
 
